8f8dfd4ac61b3b5f2dfdb02af2c5dee89a10d42d
ttaa-glitch <zbh124420@QQ.COM>
2026-09-05 14:05:15 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
+  base: '/teacher-message-board01/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
