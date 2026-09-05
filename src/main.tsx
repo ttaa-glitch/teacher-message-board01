e1528ae5a00fdf5f4fbb0da3725ba33fbd75ad02
ttaa-glitch <zbh124420@QQ.COM>
2026-09-05 14:29:11 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <Toaster position="top-center" richColors closeButton />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </HashRouter>
+  </React.StrictMode>
 );
